1abd07ea357734df0bcddccae97067927263e690
jiayingy <jiayinglek@gmail.com>
2019-10-19 20:17:16 +0800

    Create app header component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TodoAppHeader/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoAppHeader from './components/TodoAppHeader.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    TodoAppHeader
   }
 }
 </script>
---
+++ src/components/TodoAppHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1 class="todo-header">
+    <span>h</span>
+    <span>e</span>
+    <span>l</span>
+    <span>l</span>
+    <span>o</span>
+    </h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.todo-header {
+  font-size: 100px;
+  text-align: center;
+  font-weight: bold;
+  /* letter-spacing: 7px; */
+  color: #D10E7A;
+  margin: 20px 0;
+}
+</style>
