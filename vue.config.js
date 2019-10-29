fe5a89270993a04193fa467b98aca82373c49cd0
jiayingy <jiayinglek@gmail.com>
2019-10-30 01:38:06 +0800

    Config: Add eslint plugin for vue to config auto fix

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,15 @@
 const path = require('path')
 
 module.exports = {
+  lintOnSave: process.env.NODE_ENV !== 'production',
+  chainWebpack: config => {
+    config.module
+      .rule('eslint')
+      .use('eslint-loader')
+      .options({
+        fix: true,
+      });
+  },
   pluginOptions: {
     'style-resources-loader': {
       preProcessor: 'scss',
