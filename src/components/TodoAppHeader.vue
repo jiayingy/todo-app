d5c0fff72b053cdd05c619e11c496615241609e8
jiayingy <jiayinglek@gmail.com>
2019-10-30 02:53:31 +0800

    App: Compute day of week and date

--- src/components/TodoAppHeader.vue
+++ src/components/TodoAppHeader.vue
@@ -6,12 +6,40 @@
     <span>l</span>
     <span>o</span>
     </h1>
+    <h5 class="date">
+      {{ dayOfWeek }}, <span class="normal">{{ date }} {{ month }} {{ year }}</span>
+    </h5>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      selectedDate: new Date()
+    };
+  },
+  computed: {
+    dayOfWeek () {
+      return this.selectedDate.toLocaleString('en-us', {
+        weekday: 'long'
+      });
+    },
+    date () {
+      return this.selectedDate.getDate();
+    },
+    month () {
+      return this.selectedDate.toLocaleString('en-US', {
+        month: 'short'
+      });
+    },
+    year () {
+      return this.selectedDate.toLocaleString('en-US', {
+        year: 'numeric'
+      });
+    }
 }
+};
 </script>
 
 <style>
