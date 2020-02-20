2047307469d5dd26f96b2e205e1631ad7ef1c0d7
jiayingy <jiayinglek@gmail.com>
2020-02-21 01:06:48 +0800

    App: Create input for todo item

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <template>
   <div id="app">
     <TodoAppHeader />
+    <TodoInput />
   </div>
 </template>
 
 <script>
 import TodoAppHeader from './components/TodoAppHeader.vue';
+import TodoInput from './components/TodoInput.vue';
 
 export default {
   name: 'App',
   components: {
-    TodoAppHeader
+    TodoAppHeader,
+    TodoInput
   }
 };
 </script>
 
 <style lang="scss">
+#app {
+  text-align: center;
+}
 </style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <input
+    v-model="todo"
+    type="text"
+    placeholder="What would you like to work on today?"
+  >
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todo: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
