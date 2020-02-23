9f31aaa4c0643f9bc12f4a2143d642489ced152a
jiayingy <jiayinglek@gmail.com>
2020-02-24 01:20:04 +0800

    App: Emit add new to do event on enter keydown

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -3,6 +3,7 @@
     v-model="todo"
     type="text"
     placeholder="What would you like to work on today?"
+    @keydown.enter="addNewTodo"
   >
 </template>
 
@@ -12,10 +13,25 @@ export default {
     return {
       todo: ''
     }
+  },
+  methods: {
+    addNewTodo() {
+      if (this.todo) {
+        this.$emit('add', this.todo);
+      }
+    }
   }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+input {
+  border: 0;
+  outline: none;
+  padding: 5px;
+  font-size: 16px;
+  background-color: transparent;
+  width: 400px;
+  text-align: center;
+}
 </style>
