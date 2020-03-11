96830e08dabfe3f4ca7f174331ece8571b4d6413
jiayingy <jiayinglek@gmail.com>
2020-03-12 00:34:03 +0800

    App: Display number of characters left

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,18 +1,34 @@
 <template>
-  <input
-    v-model="todo"
-    type="text"
-    placeholder="What would you like to work on today?"
-    maxlength="250"
-    @keydown.enter="addNewTodo"
-  >
+  <div class="todo-input">
+    <input
+      v-model="todo"
+      type="text"
+      placeholder="What would you like to work on today?"
+      maxlength="250"
+      @keydown.enter="addNewTodo"
+    >
+    <div class="todo-chars-count">
+      {{ charsLeft }} chars left
+    </div>
+  </div>
 </template>
 
 <script>
+const TODO_MAX_CHARS = 250;
 export default {
   data() {
     return {
-      todo: ''
+      todo: '',
+      charsLeft: TODO_MAX_CHARS
+    }
+  },
+  watch: {
+    todo(val) {
+      if (val) {
+        this.charsLeft = TODO_MAX_CHARS - val.length;
+      } else {
+        this.charsLeft = TODO_MAX_CHARS;
+      }
     }
   },
   methods: {
@@ -26,18 +42,27 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.todo-input {
+width: 100%;
+}
 input {
   border: 0;
   outline: none;
   padding: 15px 0;
-  font-size: 16px;
+  font-size: 1em;
   background-color: transparent;
-  width: 400px;
   text-align: left;
-  font-family: 'Century Gothic', 'Helvetica', 'Arial', sans-serif;
   border-bottom: 1px solid #979797;
+  margin-bottom: 10px;
+  width: 100%;
 }
 ::placeholder {
   color: #D8D8D8;
+}
+
+.todo-chars-count {
+  text-align: right;
+  text-transform: uppercase;
+  font-size: 0.8em;
 }
 </style>
