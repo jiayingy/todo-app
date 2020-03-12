ec0f25d6bfe2e5daa584cf50b13d0b85e71f0e49
jiayingy <jiayinglek@gmail.com>
2020-03-13 01:57:09 +0800

    App: Create getters to retrieve todos list count

--- src/store-todos.js
+++ src/store-todos.js
@@ -2,9 +2,14 @@ export const storeTodo = {
   state: {
     list: []
   },
+  getters: {
+    count: state => {
+      return state.todos.length;
+    }
+  },
   mutations: {
     add(state, newTodo) {
       state.list.push(newTodo)
     },
-  }
+  },
 }
