946a1190931e729b9c3fa12d4a295837c8c9b5b4
jiayingy <jiayinglek@gmail.com>
2020-03-13 01:52:49 +0800

    App: Display todo list retrieved from store

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,9 +1,19 @@
 <template>
-  <div />
+  <div>
+    <TodoItem
+      v-for="(todo, index) in todoList"
+      :key="index"
+      :todo="todo"
+    />
+  </div>
 </template>
 
 <script>
+import TodoItem from './TodoItem';
 export default {
+  components: {
+    TodoItem
+  },
   props: {
     newTodo: {
       type: String,
@@ -13,28 +23,34 @@ export default {
   data() {
     return {
       db: null,
-      dbError: false
+      dbError: false,
+    }
+  },
+  computed: {
+    todoList() {
+      return this.$store.state.todos.list;
     }
   },
   created() {
-    this.createDb();
+    // this.createDb();
   },
   methods: {
-    createDb() {
-      let dbReq = indexedDB.open('todo-app', 1);
-      dbReq.onupgradeneeded = (event) => {
-        this.db = event.target.result;
-      }
+    // createDb() {
+    //   let dbReq = indexedDB.open('todo-app', 1);
+    //   dbReq.onupgradeneeded = (event) => {
+    //     this.db = event.target.result;
+    //   }
 
-      dbReq.onsuccess = (event) => {
-        this.db = event.target.result;
-      }
+    //   dbReq.onsuccess = (event) => {
+    //     this.db = event.target.result;
+    //     console.log(this.db)
+    //   }
 
-      dbReq.onerror = (event) => {
-        this.dbError = true;
-        alert('Error retrieving todo list, please try again later')
-      }
-    }
+    //   dbReq.onerror = (event) => {
+    //     this.dbError = true;
+    //     alert('Error retrieving todo list, please try again later')
+    //   }
+    // }
   }
 }
 </script>
